0995fdedfc5286d6014afdedbfca1f163e1c9ad9
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-08-12 05:03:01 +0100

    first commit

---
+++ src/main/java/dev/engripaye/opening_learning_platform_for_communities/OpeningLearningPlatformForCommunitiesApplication.java
@@ -0,0 +1,13 @@
+package dev.engripaye.opening_learning_platform_for_communities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpeningLearningPlatformForCommunitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpeningLearningPlatformForCommunitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/engripaye/opening_learning_platform_for_communities/OpeningLearningPlatformForCommunitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.engripaye.opening_learning_platform_for_communities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpeningLearningPlatformForCommunitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
